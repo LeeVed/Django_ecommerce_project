9adbccbf95e494752766f910188c1af0e8caa35d
LeeVed <leeveden2008@gmail.com>
2025-12-09 22:56:42 +0300

    models and initial migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Модель категории товаров
+    """
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Наименование',
+        help_text='Введите название категории'
+    )
+    # поле description необязательно
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание',
+        help_text='Введите описание категории'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']  # Сортировка по названию по умолчанию
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Модель товара
+    """
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Наименование',
+        help_text='Введите название товара'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        help_text='Введите описание товара'
+    )
+    # фото продукта необязательно
+    image = models.ImageField(
+        upload_to='products/',
+        blank=True,
+        null=True,
+        verbose_name='Изображение',
+        help_text='Загрузите изображение товара'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Категория',
+        help_text='Выберите категорию товара',
+        related_name='products'
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Цена за покупку',
+        help_text='Введите цену товара'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата последнего изменения'
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['-created_at', 'name']  # Сначала новые товары, потом по названию
+        indexes = [
+            models.Index(fields=['name']),  # индексация по названию товара
+            models.Index(fields=['category', 'created_at']), # составная ндексация для пагинации, фильтрации и отображения новинок
+        ]
+
+    def __str__(self):
+        return f"{self.name} - {self.price} руб."
--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,16 @@
 from pathlib import Path
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL
+from dotenv import load_dotenv
+import os
 
-from django.conf.global_settings import STATICFILES_DIRS
+load_dotenv(override=True)
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'your_secret_key'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = []
 
@@ -51,8 +55,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
@@ -79,7 +87,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
